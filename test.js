6f98ad8d559496959e16a511bc1bfd21b8c184c5
Jeff Bloom <bloo0178@gmail.com>
2019-06-11 12:33:04 -0400

    Finished 10 - Hold Shift to Check Multiple Checkboxes

--- test.js
+++ test.js
@@ -1,21 +1,46 @@
-const cityLocation = function() {
-    this.city = "Paris";
-}
+/*const cityLocation = function() {
+	this.city = "Paris";
+};
 
 cityLocation.prototype.get = function() {
-    console.log(this.city);
-}
+	console.log(this.city);
+};
 
 cityLocation.prototype.set = function(newCity) {
-    this.city = newCity;
+	this.city = newCity;
+};
+
+const myLocation = new cityLocation();
+console.log(myLocation);
+
+myLocation.get(); // output: Paris
+myLocation.set("Sydney");
+myLocation.get(); // output: Sydney
+myLocation.test = 3;
+console.log(myLocation.test);*/
+
+let test1 = "123";
+let test2 = "234";
+let test3 = 0;
+let temp = '';
+
+test3 = parseInt(test1) + parseInt(test2);
+
+console.log(test3);
+console.log((test3 + '')[0]);
+
+test3 = test3 + '';
+console.log(test3);
+
+/*let obj = {
+    a: 1,
+    b: 'test',
+    c: 3
 }
 
+console.log(obj);
+let objStr = JSON.stringify(obj);
+console.log(objStr);
+objStr = JSON.parse(objStr);
+console.log(objStr);*/
 
-  const myLocation = new cityLocation();
-  console.log(myLocation);
-  
-  myLocation.get();           // output: Paris
-  myLocation.set('Sydney');
-  myLocation.get();           // output: Sydney
-  myLocation.test = 3;
-  console.log(myLocation.test);
